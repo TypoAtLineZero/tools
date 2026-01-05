c5c548d40687c56160a84c638c66be8404f61cff
Franz Neumeister <neumeister.franz@gmx.de>
2026-01-05 21:53:49 +0100

    Removing unnecessary temp files

--- sha1-cracker/target/debug/deps/sha1_cracker-85240b2953591742.d
+++
@@ -1,5 +0,0 @@
-/home/franz/dev/tools/sha1-cracker/target/debug/deps/sha1_cracker-85240b2953591742.d: src/main.rs
-
-/home/franz/dev/tools/sha1-cracker/target/debug/deps/sha1_cracker-85240b2953591742: src/main.rs
-
-src/main.rs:
--- sha1-cracker/target/debug/sha1-cracker.d
+++
@@ -1,1 +0,0 @@
-/home/franz/dev/tools/sha1-cracker/target/debug/sha1-cracker: /home/franz/dev/tools/sha1-cracker/src/main.rs
