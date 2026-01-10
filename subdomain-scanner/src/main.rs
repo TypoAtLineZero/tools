8d58fca80980875af3983add14a2c4c52d0dd36c
Franz Neumeister <neumeister.franz@gmx.de>
2026-01-10 21:27:39 +0100

    Adapting main.rs

--- subdomain-scanner/src/main.rs
+++ subdomain-scanner/src/main.rs
@@ -1,3 +1,15 @@
-fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
+use std::thread;
+
+fn main() {
+    let mut my_vec: Vec<i64> = Vec::new();
+
+    thread::spawn(|| {
+        add_to_vec(& mut my_vec);
+    });
+
+    my_vec.push(34)
+}
+
+fn add_to_vec(vec: &mut Vec<i64>) {
+    vec.push(42);
 }
