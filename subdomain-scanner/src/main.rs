70a833b71dc3c4172a899dbdaa135e6ab829ee7a
Franz Neumeister <neumeister.franz@gmx.de>
2026-01-10 21:46:37 +0100

    Going parallel in main.rs

--- subdomain-scanner/src/main.rs
+++ subdomain-scanner/src/main.rs
@@ -1,13 +1,36 @@
 use std::thread;
+use rayon::prelude::*;
 
 fn main() {
     let mut my_vec: Vec<i64> = Vec::new();
 
-    thread::spawn(|| {
+    thread::spawn(move || {
         add_to_vec(& mut my_vec);
     });
 
-    my_vec.push(34)
+    my_vec.push(34);
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+
+    pool.install(|| {
+        let scan_result: Vec<Subdomain> = subdomains::enumrate(&http_client, target)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+
+        for subdomain in scan_result {
+            println!("{}:", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("  {}", port.port);
+            }
+
+            println!("");
+        }
+    });
 }
 
 fn add_to_vec(vec: &mut Vec<i64>) {
