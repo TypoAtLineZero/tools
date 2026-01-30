d35ac67f2984b8322de28df65eb0b53779e6b918
Franz Neumeister <neumeister.franz@gmx.de>
2026-01-30 21:24:29 +0100

    Copying source, still not compiling

--- crawler/src/spiders/mod.rs
+++ crawler/src/spiders/mod.rs
@@ -1,5 +1,11 @@
-#[async_trait]
+use crate::error::Error;
+use async_trait::async_trait;
 
+pub mod cvedetails;
+pub mod github;
+pub mod quotes;
+
+#[async_trait]
 pub trait Spider: Send + Sync {
     type Item;
 
@@ -7,3 +13,4 @@ pub trait Spider: Send + Sync {
     fn start_urls(&self) -> Vec<String>;
     async fn scrape(&self, url: String) -> Result<(Vec<Self::Item>, Vec<String>), Error>;
     async fn process(&self, item: Self::Item) -> Result<(), Error>;
+}
